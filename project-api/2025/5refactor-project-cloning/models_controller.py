13352a8cc74466c0a49e231ef66d39088f305e06
Hem Dutt Dabral <hem.dabral@gmail.com>
2025-06-26 17:05:33 +0530

    updated model controller

--- project-api/2025/5refactor-project-cloning/models_controller.py
+++ project-api/2025/5refactor-project-cloning/models_controller.py
@@ -40,9 +40,7 @@ def app_create_model(user, body, project_name):  # noqa: E501
 
     :rtype: None
     """
-    if connexion.request.is_json:
-        new_model = NewModel.from_dict(connexion.request.get_json())  # noqa: E501
-        
+    if connexion.request.is_json:              
         effective_user_id = user        
         as_org = connexion.request.args.get('as_org')                   
         if as_org: 
@@ -54,44 +52,52 @@ def app_create_model(user, body, project_name):  # noqa: E501
                 logger.error(f'user {user} is not a member of org {as_org}')
                 return Response(status=client_side_error(ErrorType.FORBIDDEN))
 
-        if os.path.exists(os.path.join(GlobalObjects.getInstance().getFSUserWorkspaceFolder(user_id=effective_user_id), project_name, "models", new_model.name)):
-            logger.error(f'model {new_model.name} already exists in project {project_name}')
+        new_model = NewModel.from_dict(connexion.request.get_json())  # noqa: E501
+        src_model = new_model.model_name_to_clone
+        dest_model = new_model.name  
+
+        # source model name check
+        if not model_exists(effective_user_id, project_name, src_model):
+            logger.error(f'model {src_model} does not exist in project {project_name}')
+            return Response(status=client_side_error(ErrorType.NOT_FOUND)) # TODO: return a more specific error message
+
+        # target model name check
+        if model_exists(effective_user_id, project_name, dest_model):
+            logger.error(f'model {dest_model} already exists in project {project_name}')
             return Response(status=client_side_error(ErrorType.CONFLICT)) # TODO: return a more specific error message
 
-        logger.debug(f'creating new model.\neffective_user_id: {effective_user_id}\nas_org: {as_org}\n new_model.name: {new_model.name}')
+        logger.debug(f'creating new model.\neffective_user_id: {effective_user_id}\nas_org: {as_org}\n dest_model: {dest_model}')
         if  new_model.model_name_to_clone != None: # clone model
-            model_name_to_clone = new_model.model_name_to_clone
-            new_model_name = new_model.name
-            logger.debug(f'cloning existing model.\neffective_user_id: {effective_user_id}\nas_org: {as_org}\n model_name_to_clone: {model_name_to_clone}\nnew_model_name: {new_model_name}')
+            logger.debug(f'cloning existing model.\neffective_user_id: {effective_user_id}\nas_org: {as_org}\n src_model: {src_model}\nnew_model_name: {dest_model}')
             
-            def clone_model(effective_user_id, project_name, model_name_to_clone, new_model_name):
-                logger.debug('> clone_model')
+            def clone_model(effective_user_id, project_name, src_model, dest_model):
+                logger.debug(f'cloning model.\neffective_user_id: {effective_user_id}\nas_org: {as_org}\n src_model: {src_model}\nnew_model_name: {dest_model}')
                 user_workspace_path = GlobalObjects.getInstance().getFSUserWorkspaceFolder(user_id=effective_user_id)
-                user_project_models_path_folder = os.path.join(user_workspace_path, project_name, "models")
+                models_dir_path = os.path.join(user_workspace_path, project_name, "models")
                 project_repo_uuid = effective_user_id
-                logger.debug(f'user_workspace_path: {user_workspace_path} \nuser_project_models_path_folder: {user_project_models_path_folder}')
+                logger.debug(f'user_workspace_path: {user_workspace_path} \nuser_project_models_path_folder: {models_dir_path}')
 
                 # Copy/create a new model entry in project-json with new model name
                 project_repo = GlobalObjects.getInstance().getFSProjectRepo(user_id=effective_user_id)            
                 project_repo = ProjectFileRepo(user_workspace_path)
 
                 try:
-                    project_repo.clone_model(project_name=project_name, clone_model_uuid_or_name=model_name_to_clone, model_uuid_or_name=new_model_name, model_owner_uuid=effective_user_id)
+                    project_repo.clone_model(project_name=project_name, clone_model_uuid_or_name=src_model, model_uuid_or_name=dest_model, model_owner_uuid=effective_user_id)
                 except Exception as e:
                     logger.exception(f'exception occurred: {e}')
                     return Response(status=409)
 
                 # Copy file-system contents
-                create_model_fs_copy(model_name_to_copy=model_name_to_clone, new_model_name=new_model_name, dest_folder=user_project_models_path_folder)
+                create_model_fs_copy(model_name_to_copy=src_model, new_model_name=dest_model, dest_folder=models_dir_path)
 
                 return Response(status=201)                
             
             # Clone model
-            if as_org and model_exists(as_org, project_name, model_name_to_clone) and not model_exists(as_org, project_name, new_model_name):
+            if as_org and model_exists(as_org, project_name, src_model) and not model_exists(as_org, project_name, dest_model):
                 if is_user_org_member(effective_user_id, as_org):   # verify that uuid is member of the as_org organization using orgs-api, for the time being set to True
-                    return clone_model(as_org, project_name, model_name_to_clone, new_model_name)
-            elif as_org is None and model_exists(effective_user_id, project_name, model_name_to_clone) and not model_exists(effective_user_id, project_name, new_model_name):   
-                    return clone_model(effective_user_id, project_name, model_name_to_clone, new_model_name)
+                    return clone_model(as_org, project_name, src_model, dest_model)
+            elif as_org is None and model_exists(effective_user_id, project_name, src_model) and not model_exists(effective_user_id, project_name, dest_model):   
+                    return clone_model(effective_user_id, project_name, src_model, dest_model)
             else:
                 return Response(status=409)
         else: # create a new model from scratch
@@ -99,7 +105,7 @@ def app_create_model(user, body, project_name):  # noqa: E501
 
             project_repo.create_model(
                 project_name=project_name,
-                model_name=new_model.name,
+                model_name=dest_model,
                 dataset_ref=Dataset(new_model.dataset.name, new_model.dataset.dataset_id),
                 model_metadata=ModelMetadata(new_model.classes, ModelType(new_model.model_type)),
                 model_target=ModelTarget(new_model.target.type, new_model.target.component, new_model.target.device),
@@ -107,11 +113,11 @@ def app_create_model(user, body, project_name):  # noqa: E501
             )
 
             user_workspace_path = GlobalObjects.getInstance().getFSUserWorkspaceFolder(user_id=effective_user_id)
-            user_project_models_path_folder = os.path.join(user_workspace_path, project_name, "models")
-            user_project_model_path_folder = os.path.join(user_project_models_path_folder, new_model.name)
+            models_dir_path = os.path.join(user_workspace_path, project_name, "models")
+            user_project_model_path_folder = os.path.join(models_dir_path, dest_model)
             user_project_training_path_folder = os.path.join(user_project_model_path_folder, "training")
             
-            os.makedirs(user_project_models_path_folder, exist_ok=True)
+            os.makedirs(models_dir_path, exist_ok=True)
             os.makedirs(user_project_model_path_folder, exist_ok=True)
             os.makedirs(user_project_training_path_folder, exist_ok=True)
 
