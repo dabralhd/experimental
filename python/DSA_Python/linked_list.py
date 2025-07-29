a87b405c817d33a6cabc8c733d8de21ed9a6cb31
Hem Dutt Dabral <hem.dabral@gmail.com>
2025-07-29 15:17:03 +0530

    updates python-dsa

--- python/DSA_Python/linked_list.py
+++ python/DSA_Python/linked_list.py
@@ -1,11 +1,12 @@
+from typing import Any
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.size = 0
 
     def append(self, data):
         node = Node(data)
@@ -16,6 +17,28 @@ class LinkedList:
             curr.next = node
         else:
             self.head = node
+        self.size = self.size + 1
+
+    def pop_last(self) -> Any:
+        curr = self.head
+        data = None
+        if not curr:
+            return
+        
+        if not curr.next:
+            self.head = None
+            data = curr.data
+        else:
+            prev = curr
+            curr = curr.next
+            while curr.next:
+                curr = curr.next
+                prev = prev.next
+            data = curr.data
+            prev.next = None
+
+        self.size = self.size - 1
+        return data        
 
     def __str__(self):
         curr = self.head
@@ -36,4 +59,7 @@ if __name__ == "__main__":
     l1.append(5)
     l1.append(6)           
 
-    print(l1) 
+    print(l1)
+
+    while l1.head:
+        print(f'size: {l1.size}, value: {l1.pop_last()}')
--- python/DSA_Python/stack.py
+++ python/DSA_Python/stack.py
@@ -1,4 +1,15 @@
 from linked_list import Node
 from linked_list import LinkedList
+from typing import Any
+class Stack(LinkedList):
+    def __init__(self):
+        super().__init__()
+    
+    def push(data: Any):
+        super.append(data)
 
-class 
+    def pop() -> Any:
+        
+
+
+    
