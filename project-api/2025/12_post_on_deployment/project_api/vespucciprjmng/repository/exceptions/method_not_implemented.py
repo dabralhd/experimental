2184544c8c3a1ad95cdbbe80ad197bf98d4e94b8
Hem Dutt Dabral <hem.dabral@gmail.com>
2025-10-30 09:53:07 +0530

    removals

--- project-api/2025/12_post_on_deployment/project_api/vespucciprjmng/repository/exceptions/method_not_implemented.py
+++
@@ -1,4 +0,0 @@
-
-class MethodNotImplemented(Exception):
-    def __init__(self, method_name:str):
-        super("Method \"" + method_name + "\"not implemented !")
--- project-api/2025/1project-model-ownership/project-api/project_api/__main__.py
+++
@@ -1,4 +0,0 @@
-from gunicorn.app.wsgiapp import run
-
-if __name__ == "__main__":
-    run()
